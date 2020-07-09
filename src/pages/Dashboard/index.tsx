988b9c0535d328d8e11fe2b26adf163c44d86692
lucas95santos <lucasa95santos@gmail.com>
2020-07-09 12:25:25 -0400

    Feature: criando um server fake para consumir as classes

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import api from '../../services/api';
 // components
 import { Performing, LastActions } from '../../components';
 // styles
@@ -21,143 +22,83 @@ import { FaPlus } from 'react-icons/fa';
 // images
 import booksSvg from '../../assets/books.svg';
 
+interface Classe {
+    id: number;
+    name: string;
+    initials: string;
+    media: number;
+    evaluations: {
+        p1: number;
+        p2: number;
+        po: number;
+        tp: number;
+        a?: number;
+        mf: number;
+    }
+}
+
 const Dashboard: React.FunctionComponent = () => {
     const { colors } = React.useContext(ThemeContext);
+    const [classes, setClasses] = React.useState<Classe[]>();
+
+    React.useEffect(() => {
+        api.get('classes').then(response => {
+            setClasses(response.data);
+        });
+    }, []);
+
+    const generateDelay = (index: number) => {
+        const totalClasses = classes!.length + 1;
+        const initialDelay = 0.3;
+
+        return String((totalClasses - index) * initialDelay);
+    }
 
     return (
         <Container>
             <Section>
-                <Card
-                    width="20%"
-                    height={250}
-                    image={booksSvg}
-                    animationName="SlideInRight"
-                    isClass
-                    duration="1.5s"
-                    delay="1.2s"
-                >
-                    <CardTitle>IA - Inteligência artificial</CardTitle>
-                    <CardDivider />
+                {classes?.map((classe, index) => (
+                    <Card
+                        key={classe.id}
+                        width="20%"
+                        height={250}
+                        image={booksSvg}
+                        animationName="SlideInRight"
+                        isClass
+                        duration="1.5s"
+                        delay={`${generateDelay(index+1)}s`}
+                    >
+                        <CardTitle>{`${classe.initials} - ${classe.name}`}</CardTitle>
+                        <CardDivider />
 
-                    <CardContent>
-                        <CardContentItem>
-                            <p><span>p1: </span>10.0</p>
-                            <p><span>p2: </span>10.0</p>
-                            <p><span>po: </span>10.0</p>
-                            <p><span>tp: </span>10.0</p>
-                        </CardContentItem>
-                        <CardContentItem>
-                            <p><span>mf: </span>10.0</p>
-                            <CardMessage color={colors.success}>
-                                <span>Passou com folga.<br />Bom trabalho!</span>
-                            </CardMessage>
-                        </CardContentItem>
-                    </CardContent>
+                        <CardContent>
+                            <CardContentItem>
+                                <p><span>p1: </span>{classe.evaluations.p1}</p>
+                                <p><span>p2: </span>{classe.evaluations.p2}</p>
+                                <p><span>po: </span>{classe.evaluations.po}</p>
+                                <p><span>tp: </span>{classe.evaluations.tp}</p>
+                                {classe.evaluations.a && <p><span>a: </span>{classe.evaluations.a}</p>}
+                            </CardContentItem>
+                            <CardContentItem>
+                                <p><span>mf: </span>{classe.evaluations.mf}</p>
+                                <CardMessage color={colors.success}>
+                                    <span>Passou com folga.<br />Bom trabalho!</span>
+                                </CardMessage>
+                            </CardContentItem>
+                        </CardContent>
 
-                    <CardInfo>
-                        <span>A média para essa disciplina é 6.0</span>
-                    </CardInfo>
-                </Card>
-                <Card
-                    width="20%"
-                    height={250}
-                    image={booksSvg}
-                    animationName="SlideInRight"
-                    isClass
-                    duration="1.5s"
-                    delay="0.9s"
-                >
-                    <CardTitle>FUNDRED - Fund. de Redes</CardTitle>
-                    <CardDivider />
-
-                    <CardContent>
-                        <CardContentItem>
-                            <p><span>p1: </span>10.0</p>
-                            <p><span>p2: </span>10.0</p>
-                            <p><span>po: </span>10.0</p>
-                            <p><span>tp: </span>10.0</p>
-                        </CardContentItem>
-                        <CardContentItem>
-                            <p><span>mf: </span>10.0</p>
-                            <CardMessage color={colors.success}>
-                                <span>Passou com folga.<br />Bom trabalho!</span>
-                            </CardMessage>
-                        </CardContentItem>
-                    </CardContent>
-
-                    <CardInfo>
-                        <span>A média para essa disciplina é 6.0</span>
-                    </CardInfo>
-                </Card>
-                <Card
-                    width="20%"
-                    height={250}
-                    image={booksSvg}
-                    animationName="SlideInRight"
-                    isClass
-                    duration="1.5s"
-                    delay="0.6s"
-                >
-                    <CardTitle>SO - Sistemas Operacionais</CardTitle>
-                    <CardDivider />
-
-                    <CardContent>
-                        <CardContentItem>
-                            <p><span>p1: </span>10.0</p>
-                            <p><span>p2: </span>10.0</p>
-                            <p><span>po: </span>10.0</p>
-                            <p><span>tp: </span>10.0</p>
-                        </CardContentItem>
-                        <CardContentItem>
-                            <p><span>mf: </span>10.0</p>
-                            <CardMessage color={colors.success}>
-                                <span>Passou com folga.<br />Bom trabalho!</span>
-                            </CardMessage>
-                        </CardContentItem>
-                    </CardContent>
-
-                    <CardInfo>
-                        <span>A média para essa disciplina é 6.0</span>
-                    </CardInfo>
-                </Card>
-                <Card
-                    width="20%"
-                    height={250}
-                    image={booksSvg}
-                    animationName="SlideInRight"
-                    isClass
-                    duration="1.5s"
-                    delay="0.3s"
-                >
-                    <CardTitle>MEDSOFT - Medição de Software</CardTitle>
-                    <CardDivider />
-
-                    <CardContent>
-                        <CardContentItem>
-                            <p><span>p1: </span>10.0</p>
-                            <p><span>p2: </span>10.0</p>
-                            <p><span>po: </span>10.0</p>
-                            <p><span>tp: </span>10.0</p>
-                        </CardContentItem>
-                        <CardContentItem>
-                            <p><span>mf: </span>10.0</p>
-                            <CardMessage color={colors.success}>
-                                <span>Passou com folga.<br />Bom trabalho!</span>
-                            </CardMessage>
-                        </CardContentItem>
-                    </CardContent>
-
-                    <CardInfo>
-                        <span>A média para essa disciplina é 6.0</span>
-                    </CardInfo>
-                </Card>
+                        <CardInfo>
+                            <span>{`A média para essa disciplina é ${classe.media}`}</span>
+                        </CardInfo>
+                    </Card>
+                ))}
                 <AddCard
                     width="20%"
                     height={250}
                     animationName="SlideInLeft"
                     isClass
                     duration="1.5s"
-                    delay="0.3s"
+                    delay="1.5s"
                 >
                     <AddCardCircle title="Adicionar disciplina">
                         <FaPlus />
